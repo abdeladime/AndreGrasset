d501983fae2f292b7cc1a68e39b24efed77b81b1
eleves <0004>
2017-01-26 12:25:52 -0500

    second commit (change gradle)

--- src/main/java/com/example/eleves/andregrasset/SurveyActivity.java
+++ src/main/java/com/example/eleves/andregrasset/SurveyActivity.java
@@ -1,7 +1,7 @@
 package com.example.eleves.andregrasset;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SurveyActivity extends AppCompatActivity {
 
--- src/test/java/com/example/eleves/andregrasset/ExampleUnitTest.java
+++ src/test/java/com/example/eleves/andregrasset/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.example.eleves.andregrasset;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
